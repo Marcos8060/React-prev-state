452ed580066a4bc42f3046626ebae111124d90a5
Marcos8060 <marcosgav80@gmail.com>
2022-05-24 11:29:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect,useRef} from 'react';
 
 function App() {
+  const [ count,setCount] = useState(0)
+
+  const prevCount = useRef();
+
+  useEffect(() =>{
+    prevCount.current = count
+  },[count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Now: {count} Previous: {prevCount.current}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
   );
 }
